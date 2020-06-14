69b3069184b4811f8c79064fba56ad4bcfd161bc
idichan <idichan@protonmail.com>
2020-06-14 00:58:44 -0700

    cleaned up variables.tf

--- ec2-spot/variables.tf
+++ ec2-spot/variables.tf
@@ -5,8 +5,7 @@ variable "subnetBCIDR" {}
 variable "subnetCCIDR" {}
 variable "subnetDCIDR" {}
 variable "vpc_id" {}
-variable "accessip" {
-}
+
 variable "key_name" {}
 variable "cloudwatch_retention" {
   default = 30
@@ -15,8 +14,6 @@ variable "cloudwatch_loggroup_name" {}
 variable "sub_volume_size" {}
 variable "root_volume_size" {}
 variable "project_name" {}
-variable "pvt_key" {}
-
 
 variable "environment" {}
 variable "region" {}
@@ -28,10 +25,6 @@ variable "ec2_instance_type" {}
 variable "fromport" {}
 variable "toport" {}
 variable "spot_price" {}
-variable "asg_count" {}
-variable "asg_min_size" {}
-variable "asg_des_size" {}
-variable "asg_max_size" {}
 variable "ec2_depends_on" {
   default = null
 }
