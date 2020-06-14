549ee722a418dce8449557b9098e9a4b79ba379c
idichan <idichan@protonmail.com>
2020-06-14 13:36:25 -0700

    port 80 egress for the spots

--- ec2-spot/main.tf
+++ ec2-spot/main.tf
@@ -165,6 +165,16 @@ resource "aws_security_group" "sg_base" {
     "0.0.0.0/0"]
   }
 
+  #ubuntu package update
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+
   #SSH
   ingress {
     from_port = 22
